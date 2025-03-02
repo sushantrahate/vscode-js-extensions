d0d45cb44f1a34848a7bfb43649758ce01963dad
Sushant Rahate <sushantrahate15@gmail.com>
2025-03-02 19:07:56 +0530

    updated extensions

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
-  fetch('plugins.json')
+  fetch('extensions.json')
     .then((response) => response.json())
     .then((categories) => {
-      const list = document.getElementById('plugin-list');
+      const list = document.getElementById('extensions-list');
       list.innerHTML = categories
         .map(
           (category) => `
@@ -11,16 +11,16 @@ document.addEventListener('DOMContentLoaded', function () {
                 category.category
               }</h1>
               <div class="flex flex-wrap gap-8 justify-content justify-start max-w-5xl mx-auto">
-                ${category.plugins
+                ${category.extensions
                   .map(
-                    (plugin) => `
+                    (extension) => `
                       <div class="bg-[#fff6c3] p-4 elevation-1 rounded-2xl flex items-start space-x-4 w-[320px] border border-stone-100">
-                        <img src="${plugin.thumb}" alt="${plugin.name}" class="w-12 h-12 rounded">
+                        <img src="${extension.thumb}" alt="${extension.name}" class="w-12 h-12 rounded">
                         <div>
-                          <h2 class="text-lg font-semibold">${plugin.name}</h2>
-                          <p class="text-stone-600 pb-1 text-sm">Author: ${plugin.author}</p>
-                          <p class="text-sm text-stone-700">${plugin.description}</p>
-                          <a href="${plugin.url}" target="_blank" class="text-blue-500 hover:underline text-sm">View on Marketplace</a>
+                          <h2 class="text-lg font-semibold">${extension.name}</h2>
+                          <p class="text-stone-600 pb-1 text-sm">Author: ${extension.author}</p>
+                          <p class="text-sm text-stone-700">${extension.description}</p>
+                          <a href="${extension.url}" target="_blank" class="text-blue-500 hover:underline text-sm">View on Marketplace</a>
                         </div>
                       </div>
                     `
@@ -32,5 +32,5 @@ document.addEventListener('DOMContentLoaded', function () {
         )
         .join('');
     })
-    .catch((error) => console.error('Error fetching plugins:', error));
+    .catch((error) => console.error('Error fetching extensions:', error));
 });
